65af2d5c56081b8c556d3945f71d856bbc80e3a2
Joachim Wallsin <joachim@wallsin.com>
2015-12-10 13:01:59 +0100

    Uppdaterar galleriet

--- js/script.js
+++ js/script.js
@@ -7,9 +7,16 @@ $(document).ready(function() {
     $this = $(this);
 
     if($this.hasClass('js-gallery-closed')) {
-      $('.js-gallery-open').find('.js-show').remove();
-      $('.js-gallery-open').find('.js-hide').removeClass('js-hide');
-      $('.js-gallery-open').addClass('js-gallery-closed').removeClass('js-gallery-open');
+      var $openThumbnail = $('.js-gallery-open');
+      $openThumbnail.find('.js-show')
+        .addClass('gallery--thumbnail', {
+          duration: 1500,
+          complete: function() {
+            $openThumbnail.find('.js-hide').removeClass('js-hide');
+            $(this).remove();
+            $openThumbnail.addClass('js-gallery-closed').removeClass('js-gallery-open');
+          }
+        });
 
       var fullImageSource = $this.attr('href');
       var thumbnail = this.children[0];
@@ -18,15 +25,31 @@ $(document).ready(function() {
         .attr('src', fullImageSource)
         .addClass('gallery--thumbnail js-hide')
         .on('load', function() {
-          $this.removeClass('js-gallery-closed').addClass('js-gallery-open')
-          $(this).removeClass('js-hide').addClass('js-show');
+          $this.removeClass('js-gallery-closed').addClass('js-gallery-open');
           $(thumbnail).addClass('js-hide');
+          $(this)
+            .removeClass('js-hide')
+            .addClass('js-show')
+            .removeClass('gallery--thumbnail', {
+              duration: 1500,
+              complete: function() {
+                $('html, body').animate({
+                  scrollTop: $(this).offset().top
+                });
+              }
+            });
         })
         .appendTo($this);
     } else {
-      $this.find('.js-show').remove();
-      $this.find('.js-hide').removeClass('js-hide');
-      $this.addClass('js-gallery-closed').removeClass('js-gallery-open');
+      $this.find('.js-show')
+        .addClass('gallery--thumbnail', {
+          duration: 1500,
+          complete: function() {
+            $this.find('.js-hide').removeClass('js-hide');
+            $(this).remove();
+            $this.addClass('js-gallery-closed').removeClass('js-gallery-open');
+          }
+        });
     }
 
     return false;
